b3c7bb0a104d03a1c9523e1791762f25ba67e15e
RohanNikumbh27 <rohannikumbh2793@gmail.com>
2024-06-01 08:05:33 +0530

    Changed name on mainpage

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="animate-bg bg-colour-custom">.</div>
       <div className="flex flex-col mx-auto z-50 mb-20">
         <h1 className="text-4xl m-10 mx-auto font-bold bg-gradient-to-r from-zinc-700  to-zinc-950 text-transparent inline-block bg-clip-text ml-12">
-          Welcome to Recipe App
+          Welcome to nextRecipes App
         </h1>
         <Link
           className="mx-auto px-5 py-3 rounded text-zinc-200 font-semibold text-lg bg-zinc-900 animateButton overflow-hidden hover:scale-95"
